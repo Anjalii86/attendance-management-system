859514d37dcc42cbab66e514735606f8f9e79d5d
Anjali <anjalichanchal.singh@gmail.com>
2024-10-08 00:26:05 +0530

    Add files via upload

---
+++ attendance/Main.java
@@ -0,0 +1,13 @@
+package attendance;
+import java.sql.*;
+
+
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Login login = new Login();
+        login.loginView();
+
+    }
+
+}
